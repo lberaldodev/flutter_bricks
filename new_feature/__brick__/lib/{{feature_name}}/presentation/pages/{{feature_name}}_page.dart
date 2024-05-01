3c074d8fc8a1fb551bda818509b00611cc9ac7ac
Lucas <lberaldo01@gmail.com>
2024-05-01 10:05:30 -0300

    feat: add initial brick with presentation layer

---
+++ new_feature/__brick__/lib/{{feature_name}}/presentation/pages/{{feature_name}}_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class {{feature_name.pascalCase()}}Page extends StatefulWidget {
+  const {{feature_name.pascalCase()}}Page ({Key? key}) : super(key: key);
+
+  @override
+  _{{feature_name.pascalCase()}}PageState createState() => _{{feature_name.pascalCase()}}PageState();
+}
+
+class _{{feature_name.pascalCase()}}PageState extends State<{{feature_name.pascalCase()}}Page> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
